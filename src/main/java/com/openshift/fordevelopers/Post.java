11386a404d7e033e63a358d7cda8350235176b21
pbkbphf <44126255+pbkbphf@users.noreply.github.com>
2022-08-25 13:58:13 -0400

    Update Post.java
    Anotger update.
    
    Another update.

--- src/main/java/com/openshift/fordevelopers/Post.java
+++ src/main/java/com/openshift/fordevelopers/Post.java
@@ -24,12 +24,12 @@ public class Post extends PanacheMongoEntity {
     }
 
     public String getTitle() {
-        return title;
+        return this.title;
         //return new StringBuilder(title).reverse().toString(); // Should be: return title;
     }
 
     public String getContent() {
-        return content;
+        return this.content;
         //return new StringBuilder(content).reverse().toString(); // Should be: return content;
     }
     public Long getTimestamp() {
